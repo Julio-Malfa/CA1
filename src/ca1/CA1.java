770e128245c0c1de3a39894ec232e00f1c6054c9
Julio-Malfa <sba23065@student.cct.ie>
2024-02-01 19:16:12 +0000

    Update CA1.java
    First commit

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -14,7 +14,7 @@ public class CA1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Fist commit just to test Git account.
     }
     
 }

0ed573bea025b797bea8e8dfaac45c25356e710f
Julio-Malfa <sba23065@student.cct.ie>
2024-02-03 22:13:23 +0000

    Read from Students.txt

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -4,6 +4,10 @@
  */
 package ca1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author julio.malfa
@@ -15,6 +19,31 @@ public class CA1 {
      */
     public static void main(String[] args) {
         // Fist commit just to test Git account.
+        //Read from students.txt
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("students.txt"));
+            String student;
+            
+            while ((student = reader.readLine()) !=null) {
+                String[] splitStr = student.split(" ");
+                String firstName = splitStr[0];
+                String secondName = splitStr[1];
+                String numofClasses = reader.readLine();
+                String studentNumber = reader.readLine();
+                
+                if (!firstName.matches("[a-zA-Z]+")){
+                    System.out.println("Invalid first name: " + firstName);
+                }
+                if (!secondName.matches("[a-zA-Z]+")) {
+                    System.out.println("Invalid second name: " + secondName);
+                }
+                //lets test if the variables created will display
+                System.out.println(firstName + " " + secondName + " " + numofClasses + " " + studentNumber);
+            }
+            reader.close();
+        } catch (IOException e){
+            System.out.println("Error reading the file: Student.txt");
+        }
     }
     
 }
